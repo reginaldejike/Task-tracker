f84862c64064ebee69fba74824c7794c5cfed163
Reginald <dono4executive@gmail.com>
2022-07-08 13:23:48 +0100

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const About = () => {
+  return (
+    <div className='about'>
+      <h4>version 1.0.0</h4>
+      <p>
+        This is task tracker application that track and update your task to help
+        keep track of your task or event
+      </p>
+      <Link to='/'>Go Back</Link>
+    </div>
+  );
+};
+
+export default About;
