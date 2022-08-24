28b0814d5fc0ed6cb1869c2ce04cb6d5cdf55889
Reginald <dono4executive@gmail.com>
2022-08-24 20:20:01 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState, useEffect } from 'react';
+import React from 'react';
 import AddTask from './components/AddTask';
 import Footer from './components/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './components/About';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [showAddtask, setShowAddTask] = useState(false);
